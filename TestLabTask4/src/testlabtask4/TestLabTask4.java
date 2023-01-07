10979896ca726a7beeec4bae38cc8e23e30d1386
IceStre4k <116694454+icesersis@users.noreply.github.com>
2023-01-07 10:43:20 +0800

    Add files via upload

---
+++ TestLabTask4/src/testlabtask4/TestLabTask4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testlabtask4;
+
+/**
+ *
+ * @author FAR
+ */
+public class TestLabTask4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new CarRental().setVisible(true);
+    }
+    
+}
